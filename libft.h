73b2a439bd04b89efa26d311d2e07863e5e5ff51
Casper Schuijt <cschuijt@student.codam.nl>
2022-10-05 15:18:41 +0200

    Fixed libft.h for norminette compliance

--- libft.h
+++ libft.h
@@ -6,11 +6,13 @@
 /*   By: cschuijt <cschuijt@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/04 16:44:15 by cschuijt      #+#    #+#                 */
-/*   Updated: 2022/10/04 16:58:29 by cschuijt      ########   odam.nl         */
+/*   Updated: 2022/10/05 15:10:51 by cschuijt      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#ifndef LIBFT_H
+# define LIBFT_H
 
 char	*ft_substr(char const *s, unsigned int start, unsigned int len);
 int		ft_strnlen(char *s, unsigned int maxlen);
+#endif

6348820d25ed9b1f1bd3ae993ef13e468a48afae
Casper Schuijt <cschuijt@student.codam.nl>
2022-10-06 11:32:03 +0200

    Add prototypes to header

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cschuijt <cschuijt@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/04 16:44:15 by cschuijt      #+#    #+#                 */
-/*   Updated: 2022/10/05 15:32:29 by cschuijt      ########   odam.nl         */
+/*   Updated: 2022/10/06 11:26:09 by cschuijt      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,9 @@
 char	*ft_substr(char const *s, unsigned int start, unsigned int len);
 int		ft_strnlen(char *s, unsigned int maxlen);
 char	*ft_strjoin(char const *s1, char const *s2);
+int		isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 #endif

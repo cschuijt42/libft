b9b8d63ba481ee13e2ef1d1bf11c03e356a8773a
Casper Schuijt <cschuijt@student.codam.nl>
2022-10-12 18:42:04 +0200

    small update to iteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,13 +6,13 @@
 /*   By: cschuijt <cschuijt@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 14:50:39 by cschuijt      #+#    #+#                 */
-/*   Updated: 2022/10/12 15:06:02 by cschuijt      ########   odam.nl         */
+/*   Updated: 2022/10/12 18:27:19 by cschuijt      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	ft_striteri(char *s, char (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
 

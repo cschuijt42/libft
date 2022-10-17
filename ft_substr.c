a2e825393affff6cb17b3a32aa2f35f015feece4
Casper Schuijt <casperschuijt@gmail.com>
2022-10-17 21:32:42 +0200

    ecks dee

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: cschuijt <cschuijt@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/04 13:54:42 by cschuijt      #+#    #+#                 */
-/*   Updated: 2022/10/15 22:18:28 by cschuijt      ########   odam.nl         */
+/*   Updated: 2022/10/17 21:32:33 by cschuijt      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	unsigned int	i;
 
 	total_length = ft_strlen(s);
+	if (start > total_length)
+		return (ft_calloc(sizeof(char), 1));
+	if ((total_length - start) < len)
+		len = (total_length - start);
 	while (start > 0 && *s)
 	{
 		s++;
 		start--;
 	}
-	if (start > total_length)
-		return (ft_calloc(sizeof(char), 1));
-	if ((total_length - start) < len)
-		len = (total_length - start);
 	pointer = malloc(sizeof(char) * (len + 1));
 	if (pointer == NULL)
 		return (NULL);

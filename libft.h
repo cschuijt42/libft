f9058f1af7778d2b2b010b263fa6fe4852600c1d
Casper Schuijt <cschuijt@student.codam.nl>
2022-11-09 17:29:58 +0100

    Add hints to all functions in the header

--- libft.h
+++ libft.h
@@ -21,6 +21,8 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
+// -- DEFAULT FUNCTIONS --
+
 // Returns the length of the given string. No NULL protection.
 size_t	ft_strlen(const char *s);
 // Strips whitespace and up to one +/- from a string and returns the first
@@ -75,27 +77,63 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 // be found, returns NULL.
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
+// -- CUSTOM FUNCTIONS --
+
+// Allocates and returns a string representation of integer n
 char	*ft_itoa(int n);
+// Splits string s by delimiter character c, returning a NULL-terminated
+// array of strings with each substring found.
 char	**ft_split(char const *s, char c);
+// Allocates a trimmed version of s1, removing characters from the front
+// and back that also occur in set.
 char	*ft_strtrim(char const *s1, char const *set);
+// Allocates and returns a string from string s which starts at index start and
+// is at most len bytes long.
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+// Allocates and returns a string, the contents of which are equal to each
+// character in string s after being run through function f. Unsigned int in
+// function prototype is the string index.
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+// Edits string s, overriding each character using function f. Unsigned int in
+// function prototype is the string index.
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+// Allocates and returns a string, the contents of which is s1 and s2 combined.
 char	*ft_strjoin(char const *s1, char const *s2);
+// Writes a single character to the given file descriptor.
 void	ft_putchar_fd(char c, int fd);
+// Writes a string to the given file descriptor.
 void	ft_putstr_fd(char *s, int fd);
+// Writes a string to the given file descriptor, adding a newline at the end.
 void	ft_putendl_fd(char *s, int fd);
+// Writes an integer to the given file descriptor.
 void	ft_putnbr_fd(int n, int fd);
 
+// -- LINKED LIST FUNCTIONS -- 
+
+// Adds list item new to the back of list lst. If lst is NULL, new becomes the
+// first element in the list.
 void	ft_lstadd_back(t_list **lst, t_list *new);
+// Adds list item new to the front of list lst.
 void	ft_lstadd_front(t_list **lst, t_list *new);
+// Calls function del on the content of each element in list lst, then frees
+// each element in the list.
 void	ft_lstclear(t_list **lst, void (*del)(void*));
+// Calls function del on the content of list element lst, then frees it.
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
+// Calls function f on the content of each list element in lst, updating it
+// as needed.
 void	ft_lstiter(t_list *lst, void (*f)(void *));
+// Returns a pointer to the last element of list lst.
 t_list	*ft_lstlast(t_list *lst);
+// Calls function f on the content of each list element in lst, creating a new
+// list with its results. Function del is required to clean up in case of
+// malloc failure.
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+// Creates a new list node with content pointer and next pointer NULL.
 t_list	*ft_lstnew(void *content);
+// Returns the amount of items in list lst.
 int		ft_lstsize(t_list *lst);
 
 int		ft_put_nbr_base(size_t nbr, char *base);
+
 #endif
